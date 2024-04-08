6d4e1f1a697319de58d59f08aa2c5139e04e0fa3
mauriciog88u1 <83040257+mauriciog88u1@users.noreply.github.com>
2024-04-08 15:58:06 -0600

    reducer

--- src/main/java/csx55/hw3/SongReducer.java
+++ src/main/java/csx55/hw3/SongReducer.java
@@ -3,18 +3,21 @@ package csx55.hw3;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 
 import java.io.IOException;
 
 public class SongReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-    private Text result = new Text();
-    private IntWritable count = new IntWritable();
+    private Text maxArtist = new Text();
+    private int maxCount = 0;
+    private MultipleOutputs<Text, IntWritable> mos;
 
     private int questionNumber;
 
     @Override
     protected void setup(Context context) throws IOException, InterruptedException {
         this.questionNumber = context.getConfiguration().getInt("question.number", 1);
+        mos = new MultipleOutputs<>(context);
     }
 
     @Override
@@ -36,12 +39,20 @@ public class SongReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
         for (IntWritable val : values) {
             sum += val.get();
         }
-        count.set(sum);
-        context.write(key, count);
+        if (sum > maxCount) {
+            maxCount = sum;
+            maxArtist.set(key);
+        }
     }
-    
+
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        if (questionNumber == 1) {
+            mos.write("QuestionOne", new Text("Question 1: Artist with maximum count"), new IntWritable(maxCount), "QuestionOne/part");
+            mos.close();
+        }
+    }
 
     private void processQuestion2(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
     }
-
 }
