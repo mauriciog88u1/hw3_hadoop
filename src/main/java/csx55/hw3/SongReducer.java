261349715ce1716b22b363529332a0a368bc6dbd
mauriciog88u1 <83040257+mauriciog88u1@users.noreply.github.com>
2024-04-08 16:06:52 -0600

    question 1

--- src/main/java/csx55/hw3/SongReducer.java
+++ src/main/java/csx55/hw3/SongReducer.java
@@ -43,15 +43,13 @@ public class SongReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
             maxCount = sum;
             maxArtist.set(key);
         }
+        String description = String.format("Artist with maximum count is %s with count %d", maxArtist.toString(), maxCount);
+        context.write(new Text(description), null);
     }
 
     @Override
     protected void cleanup(Context context) throws IOException, InterruptedException {
-        if (questionNumber == 1) {
-            String description = String.format("Question 1: Artist with maximum count is %s with count %d", maxArtist.toString(), maxCount);
-            mos.write("QuestionOne", new Text(description), null, "QuestionOne/part");
-            mos.close();
-        }
+
 
     }
     private void processQuestion2(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
