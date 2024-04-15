52864551e5a0ae1e6cc0d36737ed76451995c148
“Mauricio” <maur88@colostate.edu>
2024-04-15 15:51:27 -0600

    final q5 verfiy

--- src/main/java/csx55/hw3/q5AnalysisMapper.java
+++ src/main/java/csx55/hw3/q5AnalysisMapper.java
@@ -4,13 +4,30 @@ import java.io.IOException;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import static csx55.hw3.utils.Analysis.*;
+
 
 public class q5AnalysisMapper extends Mapper<Object, Text, Text, Text> {
 
-
+/*
+ * q5 is   What is the longest song(s)? The shortest song(s)? The song(s) of median length?
+ */
     @Override
     protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-    
+        String [] parts = value.toString().split("\\|");
+
+        try {
+            if(parts.length > DURATION_INDEX && parts.length > SONG_ID_INDEX_ANALYSIS ){
+                String duratiion = parts[DURATION_INDEX].equalsIgnoreCase("nan") ? "0" : parts[DURATION_INDEX];
+                context.write(new Text(parts[SONG_ID_INDEX_ANALYSIS]),new Text( "duration:"+duratiion));
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            
+        }
+        
+        
     }
     
 }
--- src/main/java/csx55/hw3/q5MetaMapper.java
+++ src/main/java/csx55/hw3/q5MetaMapper.java
@@ -1,15 +1,26 @@
 package csx55.hw3;
 
-import java.io.IOException;
-
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import static csx55.hw3.utils.metadata.*;
 
 public class q5MetaMapper extends Mapper<Object, Text, Text, Text> {
 
     @Override
-    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-    
+    protected void map(Object key, Text value, Context context) {
+
+        String[] parts = value.toString().split("\\|");
+
+        try {
+            if (parts.length > TITLE_INDEX) {
+                String songId = parts[SONG_ID_INDEX];
+                String artistName = parts[TITLE_INDEX];
+                context.write(new Text(songId), new Text("title:" + artistName));
+            }
+        } catch (Exception e) {
+            System.out.println("Error in q4MetaMapper: " + e.getMessage());
+        }
+
     }
-    
+
 }
